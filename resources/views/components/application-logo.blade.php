d1a26ebf942e848394926503de9ff92efb4e829a
divar jati nugroho <divar@transflynd.com>
2022-04-30 13:49:44 +0700

    layouting

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<figure {{ $attributes }}>
+    <img src="https://4.bp.blogspot.com/-SdnakRMpPzM/WhvRa4aRAuI/AAAAAAAAFco/6zGe2i--kNcz65zoHT0Kea8OlmCD2iSLQCLcBGAs/s1600/Malinau.png" width="60 em">
+</figure>
