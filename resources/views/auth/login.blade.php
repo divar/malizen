a05a158935aab50ec2be31f3e25cb634048465b6
divar jati nugroho <divar@transflynd.com>
2022-05-09 10:17:11 +0700

    push ke 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
+            <span>Malizen</span>
         </x-slot>
 
         <!-- Session Status -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ asset('fontawesome/css/all.css') }}" rel="stylesheet">
+        <link href="{{ asset('fontawesome/css/fontawesome.css') }}" rel="stylesheet">
+        <link href="{{ asset('fontawesome/css/brands.css') }}" rel="stylesheet">
+        <link href="{{ asset('fontawesome/css/solid.css') }}" rel="stylesheet">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -19,11 +23,17 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.top-navigation')
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="flex flex-row">
+                <div class="basis-1/5 box-border h-screen overflow-y-auto hidden sm:flex">
+                    @include('layouts.side-navigation')
+                </div>
+                <!-- Page Content -->
+                <div class="grow justify-center">
+                    <main>
+                        {{ $slot }}
+                    </main>
+                </div>
+            </div>
         </div>
     </body>
 </html>
